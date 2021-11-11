106b42f0353316ce7d3a0a3ede06e3defda11d71
Josema <josema.saborido@gmail.com>
2021-11-11 17:35:33 +0100

    Añadido un atributo de ruta de imagen al articulo

--- src/main/java/com/equipo2/Notion/NotionFinal/Entities/Categoria.java
+++ src/main/java/com/equipo2/Notion/NotionFinal/Entities/Categoria.java
@@ -27,7 +27,7 @@ public class Categoria {
     //Relacion One to Many con Articulos
     //@JsonManagedReference
     //@JsonIgnore
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria")
+    //@ManyToMany(fetch = FetchType.LAZY, mappedBy = "categoria")
     private List<Articulo> articulos=new ArrayList<>();
 
     //CONSTRUCTORES
